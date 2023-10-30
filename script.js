79c254eb4fa402e0ab455a3cc35adf4cee8209ed
MalKhar <brunocbranco16@gmail.com>
2023-10-30 11:37:06 -0300

    new script open wpp

---
+++ script.js
@@ -0,0 +1,13 @@
+var links = [
+    "https://wa.me/19998324619?text=Nome completo do(a) jovem:%0aNúmero de contato:%0aIdade:%0aSérie:%0aTurno da escola:%0aCidade:%0aBairro:%0aTem alguma qualificação? (Cursos de inglês, informática, técnicos...):%0aNome completo do(a) responsável:%0aNúmero de contato do(a) responsável:%0aÁrea das vagas que teria interesse:",
+    "https://wa.me/19983031454?text=Nome completo do(a) jovem:%0aNúmero de contato:%0aIdade:%0aSérie:%0aTurno da escola:%0aCidade:%0aBairro:%0aTem alguma qualificação? (Cursos de inglês, informática, técnicos...):%0aNome completo do(a) responsável:%0aNúmero de contato do(a) responsável:%0aÁrea das vagas que teria interesse:",
+    "https://wa.me/19997128243?text=Nome completo do(a) jovem:%0aNúmero de contato:%0aIdade:%0aSérie:%0aTurno da escola:%0aCidade:%0aBairro:%0aTem alguma qualificação? (Cursos de inglês, informática, técnicos...):%0aNome completo do(a) responsável:%0aNúmero de contato do(a) responsável:%0aÁrea das vagas que teria interesse:",
+    "https://wa.me/19998942458?text=Nome completo do(a) jovem:%0aNúmero de contato:%0aIdade:%0aSérie:%0aTurno da escola:%0aCidade:%0aBairro:%0aTem alguma qualificação? (Cursos de inglês, informática, técnicos...):%0aNome completo do(a) responsável:%0aNúmero de contato do(a) responsável:%0aÁrea das vagas que teria interesse:"];
+
+    function openWpp() {
+        var ranIdx = Math.random()*links.length;
+        ranIdx = parseInt(ranIdx, 10);
+        var link = links[ranIdx];
+
+        window.open(link);
+    }
